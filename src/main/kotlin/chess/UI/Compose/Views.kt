9cdc9999569cad678912152899d7e29a68cb07cb
ZeMaheli <josemcv99@hotmail.com>
2022-01-05 17:56:46 +0000

    Fixed join and open boxes

--- src/main/kotlin/chess/UI/Compose/Views.kt
+++ src/main/kotlin/chess/UI/Compose/Views.kt
@@ -19,9 +19,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.*
 import androidx.compose.ui.window.*
 import chess.Chess
 import chess.GameName
@@ -248,8 +246,8 @@ fun updateGameIfOtherPlayerMoved(chess: MutableState<Chess>) {
     ifOtherPlayerMoved(chess.value){
         chess.value = refreshBoardAction(chess.value)
     }
-
 }
+
 @Composable
 fun ifOtherPlayerMoved(chess: Chess,block: @Composable () -> Unit){
     val gameId = chess.currentGameId
@@ -260,11 +258,8 @@ fun ifOtherPlayerMoved(chess: Chess,block: @Composable () -> Unit){
 
         if(moveCount %2 != pNumber)
                 block()
-
     }
 }
-
-
 
 /**
  * Builds the UI for the background chessboard.
@@ -392,7 +387,6 @@ private fun tile(
             }
         }
     }
-
 }
 
 @Composable
@@ -405,8 +399,7 @@ private fun dealWithMovement(
     result: MutableState<Result>,
     promotionType: MutableState<String>,
     isSelectingPromotion: MutableState<Boolean>,
-
-    ) {
+){
     val finish = clicked.value as FINISH
     val board = chess.board
     val finishSquare = finish.square.toSquare()
@@ -416,10 +409,10 @@ private fun dealWithMovement(
     val finalMoveString = remember { mutableStateOf("") }
     if(promotionType.value != ""){
         finalMoveString.value =move.value + finish.square + "=" + promotionType.value
-    }else {
+    }
+    else {
         finalMoveString.value = move.value + finish.square
     }
-
         val value = playAction(finalMoveString.value, chess)
 
         result.value = value
@@ -448,7 +441,6 @@ private fun dealWithMovement(
                 clearPossibleMovesIfOptionEnabled(showPossibleMoves, possibleMovesList)
             }
         }
-
     promotionType.value = ""
 }
 
@@ -465,8 +457,6 @@ private fun BackgroundTile(tileColor: Colors) {
 
     Box(Modifier.background(color).size(TILE_SIZE))
 }
-
-
 
 @Composable
 private fun drawCoordinateNumbers() {
@@ -570,9 +560,6 @@ private fun drawVisuals(
 
 }
 
-
-
-
 /**
  * Makes the menu bar at the top of the screen
  * @param action    the action to display
@@ -666,16 +653,23 @@ private fun getGameName(
         onCloseRequest = {onClose() },
         title = "Insert Game Name",
         resizable = false,
+        state = DialogState(size = DpSize(width = 400.dp, height = 200.dp))
 
     ) {
         Column{
-            Text("Please insert the name of the game to enter")
+            Text(
+                "Please insert the name of the game to enter",
+                fontSize = 19.sp,
+                modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
+            )
             TextField(
                 value = input.value,
-                onValueChange = { input.value = it }
+                onValueChange = { input.value = it },
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 5.dp)
             )
             Button(
-                onClick = { filterGameName() }
+                onClick = { filterGameName() },
+                modifier = Modifier.absoluteOffset(150.dp,5.dp).size(100.dp,50.dp)
             ) {
                 Text("Confirm")
             }
